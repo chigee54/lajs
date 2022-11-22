a7182c0c377329c3ee0de59ec7a92313105e3a7b
chigee54 <86865792+chigee54@users.noreply.github.com>
2022-11-22 16:23:11 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ data_path=cail2022_类案检索_封测阶段/
 python predict.py \
     --input $data_path \
     --output ./ \
-    --encoder_path saved/bsz1_lr1e-05/lawformer_best.pt \
-    --rank_mlp_path saved/bsz1_lr5e-05/rank_mlp_best.pt \
+    --encoder_path saved/bsz1_lr1e-05/Lawformer.pt \
+    --rank_mlp_path saved/bsz1_lr1e-04/RankAttention.pt \
     --model_path thunlp/Lawformer \
     --tokenizer_path hfl/chinese-roberta-wwm-ext \
     --extract_batch_size 10 \
